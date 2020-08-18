94a3ee0de0fc745311b4436a531cbf282a4da42a
karloPehar <karlopehar1@hotmail.com>
2020-08-18 14:53:52 +0100

    dodavanje ProizvodID u ProizvodVM; task 21

--- ClassLibrary1/ViewModels/ProizvodVM.cs
+++ ClassLibrary1/ViewModels/ProizvodVM.cs
@@ -6,6 +6,8 @@ namespace ClassLibrary1.ViewModels
 {
     public class ProizvodVM
     {
+        public int ProizvodID { get; set; }
+        
         public string nazivProizvoda { get; set; }
 
         public string opisProizvoda { get; set; }
