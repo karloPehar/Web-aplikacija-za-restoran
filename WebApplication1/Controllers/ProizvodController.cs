7d9965fdfbb08976a9417037b04ad7b9d24a55c0
karloPehar <karlopehar1@hotmail.com>
2020-08-20 02:45:44 +0100

    dorada funkcionalnosti za prikazivanje ponude; task 24

---
+++ WebApplication1/Controllers/ProizvodController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ClassLibrary1.Models;
+using ClassLibrary1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class ProizvodController : Controller
+    {
+        public IActionResult Ponuda()
+        {
+            mojDbContext db = new mojDbContext();
+
+            List<ProizvodVM> proizvodi = db.Proizvod
+                .Select(p => new ProizvodVM
+                {
+                    ProizvodID = p.ProizvodID,
+                    nazivProizvoda = p.Naziv,
+                    opisProizvoda = p.Opis,
+                    cijenaProizvoda = p.Cijena,
+                    nazivSlike = p.Slika.lokacijaSlike
+
+
+                }).ToList();
+
+            db.Dispose();
+
+            ViewData["ProizvodKljuc"] = proizvodi;
+           
+
+
+            return View();
+        }
+    }
+}
