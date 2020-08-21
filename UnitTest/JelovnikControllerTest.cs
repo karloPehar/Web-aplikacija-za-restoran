73fbfe8ab89591e3b88ec45874338620ad6ca462
karloPehar <karlopehar1@hotmail.com>
2020-08-21 01:23:58 +0100

    kreiranje testa za jelovnikController

---
+++ UnitTest/JelovnikControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication1.Controllers;
+
+namespace UnitTest
+{
+   [TestClass]
+    public class JelovnikControllerTest
+    {
+        [TestMethod]
+        public void Proizvodi_View_Not_Null()
+        {
+            JelovnikController test = new JelovnikController();
+            Assert.IsNotNull(test.Proizvodi());
+        }
+
+    }
+}
