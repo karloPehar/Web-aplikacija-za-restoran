8a1aa9d6e4b4c66a0e9e6a7502a475c8e54fa706
karloPehar <karlopehar1@hotmail.com>
2022-02-24 14:51:58 +0100

    izmjena connection stringa i dodavanje backupa baze podataka

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -56,7 +56,7 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<mojDbContext>(options=> options.UseSqlServer(Configuration.GetConnectionString("IB150198T")));
+            services.AddDbContext<mojDbContext>(options=> options.UseSqlServer(Configuration.GetConnectionString("RestoranLocalHost")));
             services.AddControllersWithViews();
 
             services.AddMvc();
